baaf751b903a7dae4c7766b6549dc9f40b7a4b65
Rutav04 <donros2.dr@gmail.com>
2024-11-15 15:56:57 -0500

    base asteroid done

--- github.com/rutav04/astroid/main.py
+++ github.com/rutav04/astroid/main.py
@@ -1,11 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -15,7 +17,9 @@ def main():
     updateable = pygame.sprite.Group()
     drawble = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updateable, drawble)
+    Shot.containers = (shots, updateable, drawble)
     Asteroid.containers =(asteroids, updateable, drawble)
     AsteroidField.containers = updateable
     asteroid_field = AsteroidField()
@@ -31,6 +35,15 @@ def main():
 
         for obj in updateable:
             obj.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
         
         screen.fill((0, 0, 0))
         
--- github.com/rutav04/astroid/player.py
+++ github.com/rutav04/astroid/player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 180
+        self.shoot_timer = 0
 
 
 # in the player class
@@ -25,20 +27,29 @@ class Player(CircleShape):
 
     
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]: 
-            self.rotate(-dt)
-        if keys[pygame.K_d]:
-            self.rotate(dt)
         if keys[pygame.K_w]:
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)  
+        if keys[pygame.K_a]: 
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.Shoot()
+       
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    
+    def Shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
